23e00d666471e8e261f18a02db588c97f8e46c55
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-03-03 17:26:05 +0900

    バリデーション設定等

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true do
+          validates :last_name, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width characters' }
+          validates :first_name, format: { with: /\A[^ -~｡-ﾟ]+\z/, message: 'Full-width characters' }
+          validates :nickname
+        end
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+        validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 end

37a4619fbe0891675c69ee0e9630e327f7ddcc8d
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-03-18 18:41:50 +0900

    vue動作確認

--- app/javascript/packs/costom_vue.js
+++ app/javascript/packs/costom_vue.js
@@ -3,9 +3,9 @@ import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    el: '#hello',
+    el: '#hello2',
     data: {
-      message: "Can you say hello?"
+      message: "hello hello?"
     },
     components: { App }
   })
